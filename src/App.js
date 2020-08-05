5a09490f7a52d80374caaa701bb42029fca142c9
Mousta Taenchiki <moustachfa269@gmail.com>
2020-08-05 14:28:58 -0400

    button added

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ class App extends React.Component {
     }
   }
 
+  // function handleClick(){
+  //   console.log("I was clicked");
+  //
+  // };
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
             Hello World. My name is Mousta. Here is a simple weather app.
             <h1 span className= "topStyle"> Look up any zipcode below.  </h1>
+            <p> Click me to look up weather.</p>
+            <button onClick= {this.handleClick}> Look me up</button>
         </header>
 
 

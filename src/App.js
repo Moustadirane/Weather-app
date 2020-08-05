501e475c421c926ed0d5a9a5e144d2b360757f87
Mousta Taenchiki <moustachfa269@gmail.com>
2020-08-05 13:56:16 -0400

    stage 1

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import './App.css';
 class App extends React.Component {
   constructor(){
     super()
-    this.state = {}
+    this.state = {
+      character : { }
+    }
   }
 
   render(){
     return (
       <div className="App">
         <header className="App-header">
-            Hello World.
+            Hello World. My name is Mousta. Here is a simple weather app.
+            <h1 span className= "topStyle"> Look up any zipcode below.  </h1>
         </header>
+
+
       </div>
     );
   }

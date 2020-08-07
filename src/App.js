a48a6c6534339a4c06743c7850cbad38960bad54
Mousta Taenchiki <moustachfa269@gmail.com>
2020-08-07 00:12:49 -0400

    Submit button changed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import './App.css';
+import {useState, useEffect} from 'react';
+
 
 class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      character : { }
+      userInput : " "
     }
+    //May need to bind to make "this" work
+    //this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // function handleClick(){
-  //   console.log("I was clicked");
-  //
+  handleSubmit = () => {
+  }
+
+  // handleSubmit(event) => {
+  //   return (
+  //     console.log("hello");
+  //   )
   // };
 
   render(){
     return (
       <div className="App">
         <header className="App-header">
-            Hello World. My name is Mousta. Here is a simple weather app.
-            <h1 span className= "topStyle"> Look up any zipcode below.  </h1>
+            Hello World. My name is Mousta Taenchiki. Here is a simple weather app.
+            <h1 span className= "topStyle"> Look up weather for any location below:  </h1>
 
-            <form>
+            <form onSubmit = {this.handleSubmit}>
               <label>
                 <h2>Input Zipcodes:</h2>
-                <input type = "text" name = "name" />
+                <input type = "text" placeholder = "search zipcode" name = "textBox" />
+
               </label>
               <input type = "submit" value = "Submit" />
+
             </form>
 
 
